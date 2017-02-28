83eea3ada0e3d13405e4f68787f12d5315f355d6
Roytreo28 <therenceforot@orange.fr>
2017-02-28 21:50:05 +0100

    Initial Commit

---
+++ SonarHearing/src/fr/asynchronous/sonarhearing/version/INMS.java
@@ -0,0 +1,11 @@
+package fr.asynchronous.sonarhearing.version;
+
+import org.bukkit.entity.Player;
+
+/**
+ * @author Roytreo28
+ */
+public interface INMS {
+
+	public void redAlert(Player player, Boolean bool);
+}
